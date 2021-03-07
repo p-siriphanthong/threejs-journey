74c4fde4e8cdd10647f49dfc08e87d49eff075a2
Punn Siriphanthong <p.siriphanthong@gmail.com>
2021-03-07 22:57:19 +0700

    [12] do exercise: adding a few lights

--- 12-materials/exercise/src/script.js
+++ 12-materials/exercise/src/script.js
@@ -37,6 +37,18 @@ const sizes = {
 }
 
 /**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(ambientLight)
+
+const pointLight = new THREE.PointLight(0xffffff, 0.5)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
+
+/**
  * Objects
  */
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 16))
