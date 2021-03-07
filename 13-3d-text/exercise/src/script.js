8c3cef74a91ca851300c5165c20bbc87e322fd9a
Punn Siriphanthong <p.siriphanthong@gmail.com>
2021-03-08 01:23:30 +0700

    [13] do exercise: create the geometry

--- 13-3d-text/exercise/src/script.js
+++ 13-3d-text/exercise/src/script.js
@@ -26,18 +26,23 @@ const textureLoader = new THREE.TextureLoader()
 const fontLoader = new THREE.FontLoader()
 
 fontLoader.load('/fonts/helvetiker_regular.typeface.json', font => {
-  console.log('loaded', font)
+  const textGeometry = new THREE.TextGeometry('Hello Three.js', {
+    font: font,
+    size: 0.5,
+    height: 0.2,
+    curveSegments: 12,
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 5,
+  })
+  const textMaterial = new THREE.MeshBasicMaterial({ wireframe: true })
+  const text = new THREE.Mesh(textGeometry, textMaterial)
+  scene.add(text)
+
+  textGeometry.computeBoundingBox()
 })
-
-/**
- * Object
- */
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial()
-)
-
-scene.add(cube)
 
 /**
  * Sizes
