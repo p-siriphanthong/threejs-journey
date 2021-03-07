790a08b54e79aa7d9823e195e7698aa461619eda
Punn Siriphanthong <p.siriphanthong@gmail.com>
2021-03-08 01:41:47 +0700

    [13] do exercise: add donut objects

--- 13-3d-text/exercise/src/script.js
+++ 13-3d-text/exercise/src/script.js
@@ -59,6 +59,29 @@ fontLoader.load('/fonts/helvetiker_regular.typeface.json', font => {
   const textMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
   const text = new THREE.Mesh(textGeometry, textMaterial)
   scene.add(text)
+
+  /**
+   * Donuts
+   */
+  for (let i = 0; i < 100; i++) {
+    const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
+    const donutMaterial = new THREE.MeshMatcapMaterial({
+      matcap: matcapTexture,
+    })
+    const donut = new THREE.Mesh(donutGeometry, donutMaterial)
+
+    donut.position.x = (Math.random() - 0.5) * 10
+    donut.position.y = (Math.random() - 0.5) * 10
+    donut.position.z = (Math.random() - 0.5) * 10
+
+    donut.rotation.x = Math.random() * Math.PI
+    donut.rotation.y = Math.random() * Math.PI
+
+    const scale = Math.random()
+    donut.scale.set(scale, scale, scale)
+
+    scene.add(donut)
+  }
 })
 
 /**
